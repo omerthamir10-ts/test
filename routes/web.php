82ff70467f92c67687aad9867420b47275d0537f
omerthamir10-ts <omerthamir10@email.com>
2026-06-15 10:02:41 +0300

    fix register link

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DebtController;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
 });
